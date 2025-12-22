72e5603c7e01baa1f6e6e0d6864c42f4fade54f6
Gabriele Prestano <gabryprestanoitaly@gmail.com>
2025-12-22 10:38:09 +0100

    Added Hero Section

---
+++ frontend/src/app/features/home/components/hero-section/hero-section.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Container } from '@/app/layout/container/container';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [Container],
+  templateUrl: './hero-section.html',
+  styleUrl: './hero-section.css',
+})
+export class HeroSection {}
